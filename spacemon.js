0ece636e49871cda95dca4c76f692755c46466d6
Rod Vagg <rod@vagg.org>
2024-04-26 19:17:39 +1000

    lots more fixes, compile ddo stats down to single file

--- spacemon.js
+++ spacemon.js
@@ -1,40 +1,61 @@
-// import { encode as jsonEncode } from '@ipld/dag-json'
+import { join as pathJoin } from 'node:path'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { errors as undiciErrors } from 'undici'
 import { loadConfig } from './lib/config.js'
-import { createApi } from './lib/lotus.js'
+import { createApi, MaxResultsError } from './lib/lotus.js'
 import { createStore } from './lib/store.js'
 import { collectExtraState, allEventTypes } from './lib/builtin-actor-events.js'
+import { compileDDOData } from './lib/compile-ddo.js'
 
 /**
  * @typedef {import('./src/store.js').Store} Store
  */
 
-const nv22Epoch = {
-  // calibnet: 1493854, // the fixed height, nv22 was technically before this but the events weren't complete
-  calibnet: 1427974, // the original nv22 height on calibnet, before fix
-  mainnet: 3855360
+const networkParams = {
+  mainnet: {
+    epoch0Time: new Date('2020-08-24T22:00Z'),
+    nv22Epoch: 3855360
+  },
+  calibnet: {
+    epoch0Time: new Date('2022-11-01T18:13Z'),
+    nv22Epoch: 1427974
+    // nv22Epoch: 1493854, // the fixed height, nv22 was technically before this but the events weren't complete
+  }
 }
 const finalityEpochs = 900 // don't consider events from epochs newer than this, reverts are hard
 const epochsPerDay = 2880
-const maxFilterEpochRange = Math.floor(epochsPerDay / 2) // default in lotus is 2880, our API calls will likely fail for larger than this
-const loopPauseTime = 1000 * 10 // 10 seconds
+const maxFilterEpochRange = Math.floor(epochsPerDay / 12) // default in lotus is 2880, our API calls will likely fail for larger than this
+const loopPauseSeconds = 60 // 60 seconds
 
-const config = await loadConfig('./config.json')
+yargs(hideBin(process.argv))
+  .command('collect [config]', 'collect events from a node', () => {}, cmdCollect)
+  .command('compile-ddo [config]', 'compile DDO-related events already collected in a store', () => {}, cmdCompileDDO)
+  .option('config', {
+    describe: 'config.json file to load',
+    default: 'config.json'
+  })
+  .help()
+  .fail(false)
+  .strictCommands()
+  .parseAsync().catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
 
-cmdCollect().catch(e => {
-  console.error(e)
-  process.exit(1)
-})
-
-async function cmdCollect () {
+async function cmdCollect (argv) {
+  const config = await loadConfig(argv.config)
+  console.log('Collecting events from', config.network, '...')
   const rpc = await createApi(config)
 
   // TODO: we need a way to start later, a node is unlikely to have all historical events from nv22
   // unless they are an archive node
-  if (nv22Epoch[config.network] == null) {
-    throw new Error(`No nv22 epoch defined for network ${config.network}`)
+  if (networkParams[config.network] == null) {
+    throw new Error(`Network parameters not available for ${config.network}`)
   }
-  const { store, latestEpoch } = await createStore(config)
-  let startEpoch = nv22Epoch[config.network]
+  const store = await createStore(config, true)
+  const latestEpoch = await store.latestEpoch()
+  let startEpoch = networkParams[config.network].nv22Epoch
   if (latestEpoch != null) {
     startEpoch = Math.max(latestEpoch + 1, startEpoch)
   }
@@ -43,23 +64,46 @@ async function cmdCollect () {
   const eventTypes = allEventTypes
   console.log('Starting at', startEpoch, 'looking for', eventTypes.join(', '))
 
+  let filterEpochRange = maxFilterEpochRange
+  let currentHeight, latestHeight, endEpoch
   while (true) {
-    const currentHeight = (await rpc.chainHead()).Height
-    const latestHeight = currentHeight - finalityEpochs
-    const endEpoch = Math.min(startEpoch + maxFilterEpochRange, latestHeight)
-    if (endEpoch <= startEpoch) {
-      await new Promise(resolve => setTimeout(resolve, loopPauseTime))
-      continue
-    }
     try {
-      await collectRange(store, rpc, startEpoch, endEpoch, eventTypes)
+      currentHeight = (await rpc.chainHead()).Height
+      latestHeight = currentHeight - finalityEpochs
+      endEpoch = Math.min(startEpoch + filterEpochRange, latestHeight)
+      if (endEpoch <= startEpoch) {
+        await sleep(loopPauseSeconds)
+        continue
+      }
+      const saved = await collectRange(store, rpc, startEpoch, endEpoch, eventTypes)
+      console.log(`Saved ${saved} events from ${startEpoch} to ${endEpoch} (current=${currentHeight}, finality=${latestHeight})`)
+      startEpoch = endEpoch + 1
     } catch (e) {
-      console.error('Error collecting range, waiting to retry ...', startEpoch, endEpoch, e.message)
-      console.error(e.stack)
-      await new Promise(resolve => setTimeout(resolve, loopPauseTime))
+      if ((e instanceof undiciErrors.UndiciError) || ['ECONNRESET', 'ECONNREFUSED'].includes(e.code)) {
+        // If we get a network error then it should be before we save any events, so we can start
+        // again. If it's an another kind of error we should probably restart and rewind.
+        console.error(`Error collecting events (start=${startEpoch}), waiting to retry: ${e.message}`)
+        await sleep(loopPauseSeconds)
+      } else if (e instanceof MaxResultsError) {
+        console.error(`Max results reached for GetActorEventsRaw (start=${startEpoch}, end=${endEpoch}), adjusting filter range`)
+        filterEpochRange = Math.floor(filterEpochRange / 2)
+        if (filterEpochRange < 5) {
+          throw new Error('Filter epoch range is too small, exiting')
+        }
+      } else {
+        throw e
+      }
     }
-    startEpoch = endEpoch + 1
   }
+}
+
+async function cmdCompileDDO (argv) {
+  const config = await loadConfig(argv.config)
+  console.log('Compiling events from', config.network, '...')
+  const store = await createStore(config, false)
+  const storePath = pathJoin(config.storePath, config.network)
+  const epoch0Time = networkParams[config.network].epoch0Time
+  await compileDDOData(store, storePath, epoch0Time)
 }
 
 /**
@@ -67,48 +111,21 @@ async function cmdCollect () {
  * @param {number} startEpoch - inclusive
  * @param {number} endEpoch - inclusive
  * @param {string[]} eventTypes
+ * @returns {Promise<number>} - number of events saved
  */
 async function collectRange (store, rpc, startEpoch, endEpoch, eventTypes) {
-  let saveCount = 0
+  let saved = 0
   for await (const { rawEvent, event } of rpc.builtinActorEvents(startEpoch, endEpoch, eventTypes)) {
     const extra = await collectExtraState(event, rpc)
     if (extra != null) {
       event.extra = extra
     }
-    /*
-    if (typeof event.event.sector === 'number') {
-      // Don't use event.tipsetKey because many Lotus APIs load state from the parent of a tipset
-      // when you request a specific tipset, so let's move ahead by 1 epoch and use that tipset.
-      const tipset = (await rpc.chainGetTipSetByHeight(event.height + 1)).Cids
-      const addr = typeof event.event.provider === 'string' ?
-        event.event.provider :
-        typeof event.event.provider === 'number' ?
-          `${config.network === 'calibnet' ? 't' : 'f'}0${event.event.provider}` :
-          event.emitter
-      // console.log(event)
-      const sectorInfo = await rpc.stateSectorGetInfo(addr, event.event.sector, tipset)
-      if (sectorInfo != null) {
-        console.log(sectorInfo)
-      }
-    }
-    */
     await store.save(rawEvent, event)
-    if (++saveCount % 100 === 0) {
-      console.log('Saved', saveCount, 'events')
-    }
-    // console.log(dagJsonPretty(event))
-    // console.log(dagJson(event))
+    saved++
   }
+  return saved
 }
 
-/*
-function dagJson (obj) {
-  return new TextDecoder().decode(jsonEncode(obj))
+async function sleep (seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000))
 }
-
-// dagJsonPretty is a minor hack to get the dag-json encoder to round-trip through the standard JSON
-// encoder so we can pretty print the output while retaining proper CID and Bytes forms.
-function dagJsonPretty (obj) {
-  return JSON.stringify(JSON.parse(new TextDecoder().decode(jsonEncode(obj))), null, 2)
-}
-*/
